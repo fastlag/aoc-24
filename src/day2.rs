e74431629ae63cd138e87c862099787e97aadc36
Filip Hedén <--help>
2024-12-03 10:49:43 +0100

    day2b complete (non-functional-style :()

--- src/day2.rs
+++ src/day2.rs
@@ -5,26 +5,33 @@ use std::path::Path;
 pub fn run() {
     if let Ok(reports) = read_input() {
         println!(
-            "{:?}",
+            "a={:?}",
             reports
                 .iter()
-                .map(|report| report_change_safety(report).iter().all(|&x| x) as i32)
+                .map(|report| report_safe(report) as i32)
+                .sum::<i32>()
+        );
+        println!(
+            "b={:?}",
+            reports
+                .iter()
+                .map(|report| report_almost_safe(&report) as i32)
                 .sum::<i32>()
         );
     }
 }
 
-fn report_change_safety(report: &Vec<i32>) -> Vec<bool> {
+fn report_safe(report: &Vec<i32>) -> bool {
     return report
         .windows(2)
         .map(|pair| pair[1] - pair[0])
         .collect::<Vec<i32>>()
         .windows(2)
-        .map(|pair| safe(pair[0], pair[1]))
-        .collect();
+        .map(|pair| safe_diff(pair[0], pair[1]))
+        .all(|x| x);
 }
 
-fn safe(left: i32, right: i32) -> bool {
+fn safe_diff(left: i32, right: i32) -> bool {
     if left.abs() > 3 || right.abs() > 3 {
         return false;
     } else if left.abs() < 1 || right.abs() < 1 {
@@ -34,6 +41,20 @@ fn safe(left: i32, right: i32) -> bool {
     } else {
         return true;
     }
+}
+
+fn report_almost_safe(report: &Vec<i32>) -> bool {
+    if report_safe(report) {
+        return true;
+    }
+    for i in 0..report.len() {
+        let mut tmp_report = report.clone();
+        tmp_report.remove(i);
+        if report_safe(&tmp_report) {
+            return true;
+        }
+    }
+    return false;
 }
 
 fn read_input() -> io::Result<Vec<Vec<i32>>> {
