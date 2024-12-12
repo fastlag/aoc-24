149c956bac38191611d197ef1607b25fd09d09a7
Filip Hedén <--help>
2024-12-12 11:00:35 +0100

    day12b solved

--- src/day12.rs
+++ src/day12.rs
@@ -7,7 +7,14 @@ pub fn run() {
     if let Ok(map) = read_input() {
         println!(
             "\na={:?}",
-            &areas_and_perimiters(map)
+            areas_and_perimiters(&map)
+                .iter()
+                .map(|&(area, perimiter)| area * perimiter)
+                .sum::<usize>()
+        );
+        println!(
+            "\nb={:?}",
+            areas_and_sides(&map)
                 .iter()
                 .map(|&(area, perimiter)| area * perimiter)
                 .sum::<usize>()
@@ -15,7 +22,115 @@ pub fn run() {
     }
 }
 
-fn areas_and_perimiters(map: Vec<Vec<char>>) -> Vec<(usize, usize)> {
+fn areas_and_sides(map: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
+    let regions = regions(map);
+
+    let mut areas_and_sides = Vec::new();
+    for region in regions {
+        let area = region.len();
+        let sides = calculate_sides(region.iter().map(|&(a, b, _)| (a, b)).collect());
+        areas_and_sides.push((area, sides));
+    }
+
+    return areas_and_sides;
+}
+
+fn calculate_sides(positions: Vec<(usize, usize)>) -> usize {
+    let position_set: HashSet<(usize, usize)> = positions.into_iter().collect();
+    let mut total_corners = 0; // sides == corners
+    for position in &position_set {
+        total_corners += corners(&position, &position_set);
+    }
+
+    total_corners
+}
+
+fn corners(position: &(usize, usize), position_set: &HashSet<(usize, usize)>) -> usize {
+    let mut corners = 0;
+    if position.0 > 0 {
+        let corner_a = (
+            (position.0 - 1, position.1),
+            (position.0, position.1 + 1),
+            (position.0 - 1, position.1 + 1),
+        );
+        if (position_set.contains(&corner_a.0)
+            && position_set.contains(&corner_a.1)
+            && !position_set.contains(&corner_a.2))
+            || (!position_set.contains(&corner_a.0) && !position_set.contains(&corner_a.1))
+        {
+            corners += 1;
+        }
+    } else {
+        if !position_set.contains(&(position.0, position.1 + 1)) {
+            corners += 1;
+        }
+    }
+    if position.0 > 0 && position.1 > 0 {
+        let corner_b = (
+            (position.0 - 1, position.1),
+            (position.0, position.1 - 1),
+            (position.0 - 1, position.1 - 1),
+        );
+        if (position_set.contains(&corner_b.0)
+            && position_set.contains(&corner_b.1)
+            && !position_set.contains(&corner_b.2))
+            || (!position_set.contains(&corner_b.0) && !position_set.contains(&corner_b.1))
+        {
+            corners += 1;
+        }
+    } else if position.0 == 0 && position.1 == 0 {
+        corners += 1;
+    } else if position.0 == 0 && !position_set.contains(&(position.0, position.1 - 1)) {
+        corners += 1;
+    } else if position.1 == 0 && !position_set.contains(&(position.0 - 1, position.1)) {
+        corners += 1;
+    }
+    if position.1 > 0 {
+        let corner_c = (
+            (position.0, position.1 - 1),
+            (position.0 + 1, position.1),
+            (position.0 + 1, position.1 - 1),
+        );
+        if (position_set.contains(&corner_c.0)
+            && position_set.contains(&corner_c.1)
+            && !position_set.contains(&corner_c.2))
+            || (!position_set.contains(&corner_c.0) && !position_set.contains(&corner_c.1))
+        {
+            corners += 1;
+        }
+    } else {
+        if !position_set.contains(&(position.0 + 1, position.1)) {
+            corners += 1;
+        }
+    }
+    let corner_d = (
+        (position.0, position.1 + 1),
+        (position.0 + 1, position.1),
+        (position.0 + 1, position.1 + 1),
+    );
+    if (position_set.contains(&corner_d.0)
+        && position_set.contains(&corner_d.1)
+        && !position_set.contains(&corner_d.2))
+        || (!position_set.contains(&corner_d.0) && !position_set.contains(&corner_d.1))
+    {
+        corners += 1;
+    }
+    return corners;
+}
+
+fn areas_and_perimiters(map: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
+    let regions = regions(map);
+    let mut areas_and_perimiters = Vec::new();
+    for region in regions {
+        let area = region.len();
+        let perimiter: usize = region.iter().map(|&(_, _, c)| c).sum();
+        areas_and_perimiters.push((area, perimiter));
+    }
+
+    return areas_and_perimiters;
+}
+
+fn regions(map: &Vec<Vec<char>>) -> Vec<Vec<(usize, usize, usize)>> {
     let mut regions = Vec::new();
     let mut visited = HashSet::new();
     for i in 0..map.len() {
@@ -51,15 +166,7 @@ fn areas_and_perimiters(map: Vec<Vec<char>>) -> Vec<(usize, usize)> {
             regions.push(region);
         }
     }
-
-    let mut areas_and_perimiters = Vec::new();
-    for region in regions {
-        let area = region.len();
-        let perimiter: usize = region.iter().map(|&(_, _, c)| c).sum();
-        areas_and_perimiters.push((area, perimiter));
-    }
-
-    return areas_and_perimiters;
+    return regions;
 }
 
 fn neighbours(y: usize, x: usize, max_y: usize, max_x: usize) -> Vec<(usize, usize)> {
@@ -78,8 +185,6 @@ fn neighbours(y: usize, x: usize, max_y: usize, max_x: usize) -> Vec<(usize, usi
     }
     return neighbours;
 }
-
-// position, contribution to perimiter
 
 fn read_input() -> io::Result<Vec<Vec<char>>> {
     let filename = Path::new("data/day_12_input");
@@ -115,11 +220,41 @@ mod tests {
             vec!['M', 'I', 'I', 'I', 'S', 'I', 'J', 'E', 'E', 'E'],
             vec!['M', 'M', 'M', 'I', 'S', 'S', 'J', 'E', 'E', 'E'],
         ];
-        println!("hello");
-        let result: usize = areas_and_perimiters(map)
+        let result: usize = areas_and_perimiters(&map)
             .iter()
             .map(|&(area, perimiter)| area * perimiter)
             .sum();
         assert_eq!(result, 1930);
+    }
+
+    #[test]
+    fn test_calculate_fence_cost_with_discount() {
+        let map = vec![
+            vec!['R', 'R', 'R', 'R', 'I', 'I', 'C', 'C', 'F', 'F'],
+            vec!['R', 'R', 'R', 'R', 'I', 'I', 'C', 'C', 'C', 'F'],
+            vec!['V', 'V', 'R', 'R', 'R', 'C', 'C', 'F', 'F', 'F'],
+            vec!['V', 'V', 'R', 'C', 'C', 'C', 'J', 'F', 'F', 'F'],
+            vec!['V', 'V', 'V', 'V', 'C', 'J', 'J', 'C', 'F', 'E'],
+            vec!['V', 'V', 'I', 'V', 'C', 'C', 'J', 'J', 'E', 'E'],
+            vec!['V', 'V', 'I', 'I', 'I', 'C', 'J', 'J', 'E', 'E'],
+            vec!['M', 'I', 'I', 'I', 'I', 'I', 'J', 'J', 'E', 'E'],
+            vec!['M', 'I', 'I', 'I', 'S', 'I', 'J', 'E', 'E', 'E'],
+            vec!['M', 'M', 'M', 'I', 'S', 'S', 'J', 'E', 'E', 'E'],
+        ];
+        let result: usize = areas_and_sides(&map)
+            .iter()
+            .map(|&(area, perimiter)| area * perimiter)
+            .sum();
+        assert_eq!(result, 1206);
+    }
+
+    #[test]
+    fn test_calculate_corners() {
+        let positions = vec![(0, 5), (0, 4), (1, 4), (1, 5)];
+        let position_set: HashSet<(usize, usize)> = positions.into_iter().collect();
+        for position in position_set.clone().into_iter() {
+            let result = corners(&position, &position_set);
+            assert_eq!(result, 1);
+        }
     }
 }
